94528a3e0e40d13f5e08574f961c0aa08b793e9b
Hilary Shaner <hilaryshaner@gmail.com>
2022-04-26 13:28:09 -0500

    post list working

--- src/components/Posts/PostManager.js
+++ src/components/Posts/PostManager.js
@@ -3,7 +3,6 @@ export const getPosts = () => {
         .then(res => res.json())
 };
 
-
 export const getPostById = (id) => {
     return fetch(`http://localhost:8088/posts/${id}`)
         .then(res => res.json())
