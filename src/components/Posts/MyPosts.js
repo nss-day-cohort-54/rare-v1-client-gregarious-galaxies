ffbb2377b13a3be76b24966bd3acf88afcb27a7a
Hilary Shaner <hilaryshaner@gmail.com>
2022-04-28 15:08:57 -0500

    deleting posts progress

--- src/components/Posts/MyPosts.js
+++ src/components/Posts/MyPosts.js
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-import { getPosts } from "./PostManager";
+import { deletePost, getPosts } from "./PostManager";
 
 export const MyPostList = () => {
     const [posts, setPosts] = useState([])
     let currentUserId = parseInt(localStorage.getItem("token"))
-    
+
     useEffect(() => {
         getPosts().then(postData => setPosts(postData))
     },
         []
     )
-    
+
     const myPosts = posts.filter(post => post.user_id === currentUserId)
+
+    //define a function to delete a blog post
+    //invoke the DELETE method from ApiManager and then fetch the new list of posts
+    const removePost = (id) => {
+        deletePost(id)
+            .then(()=> {
+                getPosts()
+                    .then((postData) => {
+                        setPosts(postData)
+                    })
+            })
+    }
 
     return (
         <>
@@ -28,7 +40,13 @@ export const MyPostList = () => {
                                 <p className="post__category">{post.category?.label}</p>
                             </div>
                         </article >
+                        <div>
+                            <button id="btn" className="btn-addTag" onClick={removePost} >
+                                Delete Post
+                            </button>
+                        </div>
                     </div>
+
                 })
             }
         </>
