9171378711bb5b50a0db723c954dc3ce55e978e1
Stevie Hollander <steviehollander@icloud.com>
2022-04-29 13:28:35 -0500

    searchByCategory

--- src/components/Posts/PostLIst.js
+++ src/components/Posts/PostLIst.js
@@ -2,30 +2,80 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Post } from "./Post";
 import { getPosts, searchPosts } from "./PostManager";
+import { getCategories } from "./PostManager";
 
 
 export const PostList = () => {
 
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([]) //master copy, all my posts
     const [query, setQuery] = useState("");
+    const [categories, setCategories] = useState([]);
+    const [selectedCat, setSelectedCat] = useState("")
+    const [filteredPosts, setFilteredPosts] = useState([]) //copy of posts, but safe to manipulate
 
 
-    let filteredPosts = posts.filter(post => {
-        if (query === '') {
-            return post
-        } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
-            return post
-        }
-    })
+
+    // let filteredPosts = posts.filter(post => {
+    //     if (query === '') {
+    //         return post
+    //     } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
+    //         return post
+    //     }
+    // })
+
+
 
 
 
 
     useEffect(() => {
-        getPosts().then(postData => setPosts(postData))
+        getCategories().then(catData => setCategories(catData))
     },
         []
         //only runs on initial load
+    )
+
+
+
+    useEffect(() => {
+        getPosts().then(postData => {
+            setPosts(postData)
+            setFilteredPosts(postData)
+
+        })
+    },
+        []
+        //only runs on initial load
+    )
+
+
+
+    useEffect(() => {
+
+
+        if (selectedCat.length > 0) {
+            const filteredPostByCat = posts.filter(post => post.category_id == parseInt(selectedCat))
+            setFilteredPosts(filteredPostByCat)
+
+        } else { setFilteredPosts(posts) }
+
+    },
+        [selectedCat]
+    )
+
+
+
+    useEffect(() => {
+
+
+        if (query.length > 0) {
+            const filteredPostByQuery = posts.filter(post => post.title.toLowerCase().includes(query.toLowerCase()))
+            setFilteredPosts(filteredPostByQuery)
+
+        } else { setFilteredPosts(posts) }
+
+    },
+        [query]
     )
 
     return (
@@ -50,6 +100,29 @@ export const PostList = () => {
             </div>
 
 
+            <div className="form-group">
+                <label htmlFor="categoryDrop">Search by Category</label>
+                <select
+                    required autoFocus
+                    type="text"
+                    className="form-control"
+                    onChange={
+                        (event) => {
+
+                            setSelectedCat(event.target.value)
+                        }
+
+                    }>
+                    <option value="">Category...</option>
+                    {categories.map(cat => <option value={cat.id}>{cat.label}</option>)}
+                </select>
+
+
+
+
+            </div>
+
+
 
 
 
@@ -68,6 +141,7 @@ export const PostList = () => {
                     </div>
                 })
             }
+
         </>
     )
 };
--- src/components/Posts/PostManager.js
+++ src/components/Posts/PostManager.js
@@ -46,3 +46,8 @@ export const getMyPosts = (userId) => {
         .then(res => res.json())
 };
 
+
+export const getCategories = () => {
+    return fetch("http://localhost:8088/categories")
+        .then(res => res.json())
+};
